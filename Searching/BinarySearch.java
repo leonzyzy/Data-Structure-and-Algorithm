9688208c20698ab52342e6b3d04a7989f7d7ebd1
Zhiyuan <liz27@miamioh.edu>
2020-11-20 02:13:22 -0500

    Update BinarySearch.java
    improve the binary search to handle multiple values.

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,15 +1,22 @@
 package Searching;
+import java.util.ArrayList;
 
 class BinarySearch {
     public static void main(String[] args) {
+        // test linear search for unique values
         int[] x = {1,2,3,4,5,6,7};
         int z = 7;
         int index = binarySearch(x,0,x.length-1,z);
         if (index != -1) {
-            System.out.printf("Find value %d, index: %d",z,index);
+            System.out.printf("Find value %d, index: %d\n",z,index);
         } else {
-            System.out.printf("Cannot find value %d",z);
+            System.out.printf("Cannot find value %d\n",z);
         }
+        // test linear search for multiple values
+        int[] y = {1,2,3,4,4,4,4,5,6};
+        int w = 4;
+        System.out.println(binarySearchOptimize(y,0,y.length-1,w));
+
     }
     static int binarySearch(int[] x, int left, int right, int value) {
         if (left > right) {
@@ -25,6 +32,37 @@ class BinarySearch {
             return binarySearch(x,left,mid-1,value);
         } else {
             return mid;
+        }
+    }
+    static ArrayList binarySearchOptimize(int[] x, int left, int right, int value) {
+        if (left > right) {
+            return new ArrayList();
+        }
+        int mid = (left+right)/2;
+        int midValue = x[mid];
+        if (midValue < value) {
+            // search right
+            return binarySearchOptimize(x,mid+1,right,value);
+        } else if (midValue > value) {
+            // search left
+            return binarySearchOptimize(x,left,mid-1,value);
+        } else {
+           // define a list to add all index
+           ArrayList<Integer> list = new ArrayList<>();
+           // left scan
+           int temp = mid-1;
+           while (temp >= 0 && x[temp] == value) {
+               list.add(temp);
+               temp--;
+           }
+           list.add(mid);
+           // right scan
+           temp = mid+1;
+           while (temp <= x.length-1 && x[temp] == value) {
+               list.add(temp);
+               temp++;
+           }
+           return list;
         }
     }
 }
